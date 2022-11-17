e24e9909e30ca78cc4a66aae595bcb9fbecc2a9d
kwakminjung <kwak1126mj@gmail.com>
2022-11-17 13:59:31 +0900

    localStorage 구현 및 오류수정

--- static/home.js
+++ static/home.js
@@ -1,5 +1,6 @@
 window.onload = loadData;
 
+
 function input_data(input_val) {
     var input_val = document.getElementById('todo');
     var todolist = document.getElementsByClassName("check_box");
@@ -10,7 +11,7 @@ function input_data(input_val) {
     var temp_check = document.createElement('input');
     temp_check.setAttribute('type','checkbox');
     temp_check.setAttribute('class','complete');
-
+    
     var p = document.createElement("p");
     p.innerHTML = input_val.value;
     
@@ -23,14 +24,15 @@ function input_data(input_val) {
     temp.appendChild(temp_check);
     temp.appendChild(p);
     temp.appendChild(temp_del);
-
+    
     todolist[0].appendChild(temp);
 }
 
 function loadData() {
     for(var i = 0 ; i < localStorage.length ; i++){
+        var a = localStorage.key(i);
+        var data = localStorage.getItem(a);
         input_data(data);
-        var data = localStorage.getItem(i);
     }
 }
 
@@ -40,7 +42,7 @@ function plus_click() {
 
     var li = document.getElementsByTagName('li');
 
-    localStorage.setItem(localStorage.length, li);
+    localStorage.setItem('key'+localStorage.length, li);
 
     todo_input.value=null;
 }
@@ -57,21 +59,20 @@ function minus_click(a) {
         // console.log(li);
         if(li_arr[i] === li) {
             console.log('dd');
-            localStorage.removeItem(i);
+            localStorage.removeItem(localStorage.key(i));
             li.remove();
             break;
         }
     }
-    localStorage.removeItem(localStorage.length);
+    // localStorage.removeItem(localStorage.length);
 }
 
 function all_minus() {
     var ul = document.getElementsByClassName('check_box');
     var li = ul[0].children;
-    var li_cnt = ul.childElementCount;
-    while(li_cnt>0) {
+    console.log(li);
+    while(ul[0].childElementCount>0) {
         li[0].remove();
     }
     window.localStorage.clear();
-    console.log(localStorage.length);
 }
